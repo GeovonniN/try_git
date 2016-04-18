848912b2167e3ce29fb5ca476ba810bca19c26b4
Geo <N/A>
2016-04-18 14:53:15 -0700

    Travis Prep

--- src/edu/ucsd/cs110/temperature/Temperature.java
+++ src/edu/ucsd/cs110/temperature/Temperature.java
@@ -4,9 +4,9 @@ package edu.ucsd.cs110.temperature;
  * Created by cs110xmatt on 3/11/16.
  */
 public abstract class Temperature {
-
+    public float value;
     public Temperature(float v)	{
-        value = v;
+         value = v;
     }
 
     public final float getValue()	{
